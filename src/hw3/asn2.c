d0959b406befb386d1d1aa7db80ac89e2f19d1ca
ArctiicChaos <ArctiicChaos@users.noreply.github.com>
2019-10-20 17:31:13 -0800

    lolol JK, this one is much cooler
    Its kind of tested. Still need malloc, calloc, and realloc

--- src/hw3/asn2.c
+++ src/hw3/asn2.c
@@ -4,56 +4,252 @@
 // typedef for the header
 typedef struct{
 void *mem_ptr; // the address of the whole memory block used to uniquly identify the mmap it belongs to.
-int size;
-void *start;
+size_t block_size; // size of the large block allocation
+size_t size; // Size of the block given to the user including the header
+void *start; // Start of the block of usable memory minus the header
+void *next; // pointer to the next block of user allocated memory
 } header_t;
+
+
+void *prev(void *ptr);
+void free(void *ptr);
+int test(int argc, char *argv[]);
+
+void *prev(void *ptr)
+{
+    header_t *usrmem = (header_t *)(ptr - sizeof(header_t));
+    header_t *blockmem = usrmem->mem_ptr;
+
+    header_t *prev = NULL;
+    header_t *current = blockmem;
+    while (1)
+    {
+        if (current->next == NULL)
+        {
+            return NULL;
+        }
+        prev = current;
+        current = current->next;
+        if (current->start == ptr)
+        {
+            return prev;
+        }
+    }
+    return NULL;
+}
+
+void free(void *ptr)
+{
+    header_t *usermem = (header_t *)(ptr - sizeof(header_t));
+    header_t *blockmem = usermem->mem_ptr;
+    header_t *firstmem = blockmem->next;
+
+    if (firstmem->next == NULL)
+    {
+
+        munmap(blockmem->mem_ptr, blockmem->block_size);
+
+    }else if(usermem->next == NULL)
+    {
+        void *temp = prev(ptr);
+        header_t *temp2 = (header_t *)temp;
+        temp2->next = NULL;
+        // set previous next to NULL
+    }else
+    {
+        void *temp = prev(ptr);
+        header_t *temp2 = (header_t *)temp;
+        temp2->next = usermem->next;
+        // Set previous->next = current->next
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int test(int argc, char *argv[])
+{
+    
+    size_t sz = sizeof(header_t); // Var to hold the size of the headder
+
+    // block of memory plus the size of the master header
+    // EM: Size of block to be created with 'malloc', 1000 + size of header
+    size_t t_size = 1000 + sizeof(header_t);
+
+    printf("The size of the memory header header_t is %lu\n", sz);
+
+
+    // Pull the big block of memory that will hold the header and the various user memory allocations
+    void *ptr = mmap(0, t_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    // Create a Master header pointer
+    header_t *freememory = (header_t *)ptr;
+    // Assign values of the new master header pointer
+    freememory->block_size = t_size - sizeof(header_t);
+    freememory->size = freememory->block_size;
+    freememory->mem_ptr = ptr;                              // Store the location of the start of the whole memory block
+    freememory->start = (ptr + sizeof(header_t));
+    freememory->next = NULL;
+
+
+
+
+
+    // ****** create first user blocks of memory ****
+
+    header_t *usr1 = freememory->start;
+    // Copy the master header info
+    usr1->mem_ptr = freememory->mem_ptr;
+    usr1->block_size = freememory->block_size;
+
+    // Set the size of the usr1 block
+    usr1->size = 100 - sz;
+    usr1->start = freememory->start + sz;
+    usr1->next = NULL;
+
+    // set the master headers next equal to this
+    freememory->next = (void *)usr1;
+    
+    char *count = (char *)usr1->start;
+    for (int i = 0; i < usr1->size;i++)
+    {
+        count[i] = 'a';
+    }
+
+    for (int f = 0; f < usr1->size;f++)
+    {
+        printf("%c\t", count[f]);
+    }
+
+    printf("The address of usr1: %p\n", (void *)usr1);
+    printf("The address of freememory->next: %p\n", freememory->next);
+    printf("End First user block of memory\n\n");
+
+    // ***** End first user block of memory 
+
+
+
+
+
+
+
+
+
+    // ****** create second user block of memory ****
+    printf("Start The usr2 allocation\n");
+    header_t *usr2 = freememory->start + sizeof(header_t) + usr1->size;
+    // Copy the master header info
+    usr2->mem_ptr = freememory->mem_ptr;
+    usr2->block_size = freememory->block_size;
+
+    // Set the size of the usr1 block
+    usr2->size = 100 - sz;
+    usr2->start = (void *)usr2 + sz;
+    usr2->next = NULL;
+
+    // set the master headers next equal to this
+    usr1->next = (void *)usr2;
+    
+    char *count2 = (char *)usr2->start;
+    for (int o = 0; o < usr2->size;o++)
+    {
+        count2[o] = 'b';
+    }
+
+    for (int k = 0; k < usr2->size;k++)
+    {
+        printf("%c\t", count2[k]);
+    }
+
+    printf("The address of usr2: %p\n", (void *)usr2);
+    printf("The address of usr1->next: %p\n", usr1->next);
+    printf("End second user block of memory\n\n");
+
+    // ***** End second user block of memory 
+
+
+
+    
+
+
+
+
+// ****** create third user block of memory ****
+    printf("Start The usr3 allocation\n");
+    header_t *usr3 = freememory->start + sizeof(header_t) + usr1->size + sizeof(header_t) + usr2->size;
+    // Copy the master header info
+    usr3->mem_ptr = freememory->mem_ptr;
+    usr3->block_size = freememory->block_size;
+
+    // Set the size of the usr1 block
+    usr3->size = 100 - sz;
+    usr3->start = (void *)usr3 + sz;
+    usr3->next = NULL;
+
+    // set the master headers next equal to this
+    usr2->next = (void *)usr3;
+    
+    char *count3 = (char *)usr3->start;
+    for (int m = 0; m < usr3->size;m++)
+    {
+        count3[m] = 'c';
+    }
+
+    for (int b = 0; b < usr3->size;b++)
+    {
+        printf("%c\t", count3[b]);
+    }
+
+    printf("The address of usr3: %p\n", (void *)usr3);
+    printf("The address of usr2->next: %p\n", usr2->next);
+    printf("End third user block of memory\n\n");
+
+    // ***** End third user block of memory 
+
+
+
+    void *theprev = prev(usr3->start);
+
+
+    printf("The prev to usr3 is %p\n", theprev);
+
+    void *testptr = usr2->start;
+
+    free(testptr);
+
+    printf("The next to usr1 is %p\n", usr1->next);
+
+    // Testing to see if I can free memory from a new pointer.
+    munmap(ptr, t_size + sizeof(header_t));
+
+    
+    return 0;
+}
+
+
+
+
+
+
+
 
 int main(int argc, char *argv[])
 {
-    // I just wanted to see how big a header_t object is
-    size_t sz = sizeof(header_t);
-    printf("The size of the memory header header_t is %lu\n", sz);
 
-    // fill a variable with the size of the header
-    size_t t_size = 90 + sizeof(header_t);
-
-    // Pull the big block of memory that will hold the header and the user memory
-    void *ptr = mmap(0, t_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
-    // Create a header pointer
-    header_t *freememory = (header_t *)ptr;
-    // Assign values the the new header pointer object
-    freememory->size = t_size - sizeof(header_t);
-    freememory->mem_ptr = ptr;
-    freememory->start = (ptr + sizeof(header_t));
-
-    // Actually pull the block of memory the user will be allowed to use
-    void* ptr2 = ptr + sizeof(header_t);
-    
-    // Create a char pointer to the user memory
-    char* msg = (char *)ptr2;
-
-    // Write something to the memory the user has
-    msg[89] = 'a';
-
-    // Print the starting address of the whole block
-    printf("The address of the start of the whole block is %p\n", ptr);
-    // Write the starting address to the memory actually given to the user
-    printf("The start of the block minus the header is %p\n", ptr2);
-
-    // Print something written to what would be the block of memory given to the user
-    printf("Wrote a char to the last element of the allocated user memory block it is: '%c'\n", msg[89]);
-
-    // Assign the memory header to a new pointer to see if we can get the header back
-    header_t *getshit = (header_t *)(ptr2 - sizeof(header_t));
-
-    printf("Size of the memory block minus the header %d\n", getshit->size);
-    printf("Assigned the whole block to a new header_t called getshit. the original pointer matches: %p\n",getshit->mem_ptr);
-    printf("getshit's start of useable memory: %p\n",getshit->start); 
-
-    // Testing to see if I can free memory from a new pointer.
-    munmap(getshit->mem_ptr, getshit->size + sizeof(header_t));
-
+    test(argc, argv);
 
     
     return 0;
